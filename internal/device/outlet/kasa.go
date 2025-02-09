36bd5185222bacd6fea3f9c04d0794ff160ab14b
colbynh <guitaristcolby@gmail.com>
2025-02-09 15:30:17 -0500

    Add debug logging

--- internal/device/outlet/kasa.go
+++ internal/device/outlet/kasa.go
@@ -1,6 +1,7 @@
 package outlet
 
 import (
+	"fmt"
 	"os/exec"
 )
 
@@ -18,9 +19,11 @@ func (k *kasaOutlet) getBrand() string {
 
 func (k *kasaOutlet) action(action string) error {
 	cmd := exec.Command("kasa", "--host", k.id, action)
+
 	_, err := cmd.Output()
 
 	if err != nil {
+		fmt.Printf("****cmd: %s\n\n *****err: %s\n", cmd, err)
 		return err
 	}
 	return nil
